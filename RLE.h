346138a46407bc10e510c1daa4a7d578ca689de8
Robin Petit <robinpetit1997@hotmail.com>
2013-12-14 18:37:01 +0100

    Update RLE.h

--- RLE.h
+++ RLE.h
@@ -5,11 +5,17 @@
 
 typedef char BYTE;
 
+#define LENGTH  255
+
 #define RLE_OK                 0
 #define RLE_FAILED_ALLOCATION -1
 #define RLE_NULL_POINTER_ARG  -2
+#define RLE_WRITING_ERROR     -3
 
 int encode_RLE(const void *, size_t, unsigned int, void **, unsigned int *, int (*)(const void *, const void *));
 int decode_RLE(const void *, size_t, unsigned int, void **, unsigned int *);
+
+int encode_file_RLE(FILE *, FILE *, size_t, int (*)(const void *, const void *));
+int decode_file_RLE(FILE *, FILE *, size_t);
 
 #endif
