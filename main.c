b3226f0b1b3696ee3c8be68f7f01f51469ab9ca9
Robin Petit <robinpetit1997@hotmail.com>
2013-12-14 13:41:00 +0100

    Update main.c
    new functions -> new test to show these.

--- main.c
+++ main.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "RLE.h"
+
+int compare_int(const void *, const void *);
+void afficher_tab(int *, unsigned int);
+
+int compare_int(const void *a, const void *b)
+{
+    return *((const int *)a) == *((const int *)b) ? 1 : 0;
+}
+
+void afficher_tab(int *tab, unsigned int len)
+{
+    printf("tableau :\n");
+    unsigned int i;
+    for(i = 0; i < len; ++i)
+        printf("%d\n", tab[i]);
+}
 
 int main(void)
 {
-    FILE *initial_file = fopen("ifile.txt", "r"),
-         *encoded_file = fopen("efile.txt", "w+"),
-         *decoded_file = fopen("dfile.txt", "w");
-    if(initial_file == NULL)
-    {
-        perror("Unable to open ifile.txt :");
-        return EXIT_FAILURE;
-    }
-    else if(encoded_file == NULL)
-    {
-        perror("Unable to open efile.txt :");
-        return EXIT_FAILURE;
-    }
-    else if(decoded_file == NULL)
-    {
-        perror("Unable to open dfile.txt :");
-        return EXIT_FAILURE;
-    }
-    printf("Compressing...\n");
-    encode_file_RLE(initial_file, encoded_file);
-    printf("DONE !\n");
-
-    rewind(encoded_file);
-
-    printf("Decompressing...\n");
-    decode_file_RLE(encoded_file, decoded_file);
-    printf("DONE !");
-
-    fclose(initial_file);
-    fclose(encoded_file);
-    fclose(decoded_file);
+    int tab[10] = {2, 2, 2, 2, 8, 8, 8, 6, 6, 6},
+        *tab_decoded = NULL;
+    BYTE *tab_RLE_e = NULL;
+    unsigned int length_RLE_e, length2;
+    
+    if(encode_RLE(tab, sizeof(int), 10, (void **)&tab_RLE_e, &length_RLE_e, compare_int) == RLE_OK)
+        if(decode_RLE(tab_RLE_e, sizeof(int), length_RLE_e/(sizeof(int)+1), (void **)&tab_decoded, &length2) == RLE_OK)
+            afficher_tab(tab_decoded, length2);
+            
+    free(tab_RLE_e);
+    free(tab_decoded);
     return EXIT_SUCCESS;
 }
 
