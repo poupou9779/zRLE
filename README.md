6849197c408961fd0de57882cc80d45a36cf0d54
Robin Petit <robinpetit1997@hotmail.com>
2014-07-13 11:56:51 +0200

    debug and clarification
    * syntax has been rethought and adapted
    * clarified return errors
    encode_RLE function has been debugged

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ zRLE
 ====
 
 zRLE is an implementation of the Run-Length Encoding.4 functions : encode a string, decode a string, encode a file, decode a file.
+
+Warning :
+* encode_RLE sin't bugged anymore
+* decode_RLE crashes l.108 when I run the program but when I try to debug with gdb, nothing goes wrong. STILL GOTTA BE CHECKED !
+* file functions aren't checked, TODO in August
--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include "RLE.h"
 
-int compare_char(const void *, const void *);
-int compare_char(const void *a, const void *b)
+#define type int
+#define compare compare_int
+
+int compare(const void *, const void *);
+int compare(const void *a, const void *b)
 {
-    return *((const char *)a) == *((const char *)b) ? 1 : 0;
+    return *((const type *)a) == *((const type *)b) ? 1 : 0;
 }
 
 int main(void)
 {
-    FILE *ifile = fopen("ifile.txt", "rb"),
+    /*FILE *ifile = fopen("ifile.txt", "rb"),
          *cfile = fopen("efile.txt", "wb+"),
          *dfile = fopen("dfile.txt", "wb");
 
@@ -23,6 +26,24 @@ int main(void)
 
     fclose(ifile);
     fclose(cfile);
-    fclose(dfile);
+    fclose(dfile);*/
+    type tab[] = {5, 5, 5, 5, 5, 2, 2, 2, 22, 2, 45, 45, 45, 65}, /*5x5 3x2 1x22 1x2 3x45 1x65*/
+        *tab_decoded = NULL;
+    BYTE *tab_RLE = NULL;
+    unsigned int n_elements_encoded,
+                 n_elements_decoded;
+    unsigned int i;
+
+    encode_RLE(tab, sizeof(int), sizeof(tab)/sizeof(int), (void **)&tab_RLE, &n_elements_encoded, compare);
+    if(decode_RLE(tab_RLE, sizeof(int), n_elements_encoded, (void **)&tab_decoded, &n_elements_decoded) != RLE_OK) {
+        printf("too bad");
+        return EXIT_FAILURE;
+    }
+
+    for(i = 0; i < n_elements_decoded; ++i)
+        if(tab[i] != tab_decoded[i])
+            printf("[i=%d] : %02d != %02d\n", i, tab[i], tab_decoded[i]);
+    getchar();
     return EXIT_SUCCESS;
 }
+
